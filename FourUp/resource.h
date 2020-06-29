15c9e6fce3978d6942c27b05cf342b4a3de7b623
Mongrelgin <43989334+Mongrelgin@users.noreply.github.com>
2020-06-29 19:12:37 +0300

    Add files via upload

---
+++ FourUp/resource.h
@@ -0,0 +1,34 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется FourUp.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_FOURUP_DIALOG               102
+#define IDR_MAINFRAME                   130
+#define IDI_HEART                       131
+#define IDI_DIAMOND                     132
+#define IDI_SPADE                       133
+#define IDI_CLUB                        134
+#define IDB_BITMAP1                     138
+#define IDB_BITMAP2                     139
+#define IDC_AMT_LEFT                    1000
+#define IDC_CARD1                       1001
+#define IDC_CARD2                       1002
+#define IDC_CARD3                       1003
+#define IDC_CARD4                       1004
+#define IDC_DEALCARDS                   1005
+#define IDC_BUTTON2                     1006
+#define IDC_CANCEL                      1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        140
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
